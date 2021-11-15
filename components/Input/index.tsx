d56c31671e45ba0db9ca71469c45f8b7355212ec
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2021-11-15 23:31:09 +0100

    make input focus on container press

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,12 +1,14 @@
 import Text from "components/Text";
 import { getColor, tailwind } from "lib/tailwind";
-import React, { useState } from "react";
-import { TextInput, View } from "react-native";
+import React, { useRef } from "react";
+import { TextInput } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 const Input = (props: any) => {
-  const [isFocus, setIsFocus] = useState(false);
+  const inputRef = useRef<TextInput>(null);
   return (
-    <View
+    <TouchableWithoutFeedback
+      onPress={() => inputRef.current?.focus()}
       style={tailwind(
         `py-1 px-3 mb-2 bg-primary-200 border-primary-800 border rounded`
       )}
@@ -15,15 +17,12 @@ const Input = (props: any) => {
         {props.label.toUpperCase()}
       </Text>
       <TextInput
-        onFocus={() => setIsFocus(true)}
-        // onBlur={() => {
-        //   setIsFocus(false);
-        // }}
+        ref={inputRef}
         style={tailwind("text-2xl pb-1 font-medium text-primary-800")}
         placeholderTextColor={getColor("primary-400")}
         {...props}
       ></TextInput>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
