9b36bae4c6aae2124341185ad126eac4b106da51
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2021-11-17 15:04:15 +0100

    make card child dynamic

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -4,7 +4,7 @@ import { tailwind } from "lib/tailwind";
 import React, { useRef } from "react";
 import { TextInput, TouchableOpacity, View } from "react-native";
 
-const Card = ({ title, value, ...props }: any) => {
+const Card = ({ title, value, children, ...props }: any) => {
   const inputRef = useRef<TextInput>(null);
   const handleFocus = () => {
     inputRef.current?.focus();
@@ -18,7 +18,7 @@ const Card = ({ title, value, ...props }: any) => {
       <Wrapper
         style={{
           borderBottomWidth: 0.25,
-          ...tailwind("py-2 px-4 bg-white border-primary-300"),
+          ...tailwind("py-2 px-4 bg-primary-100 border-primary-400"),
         }}
         {...props}
       >
@@ -26,9 +26,7 @@ const Card = ({ title, value, ...props }: any) => {
           <Text style={tailwind("text-xl flex-none text-primary-500 mr-3")}>
             {title}
           </Text>
-          <View style={tailwind("flex-1")}>
-            <Text style={tailwind("text-xl text-right")}>{value}</Text>
-          </View>
+          <View style={tailwind("flex-1 items-end")}>{children}</View>
           {props.onPress ? (
             <View style={tailwind("flex-none ml-3")}>
               <FontAwesome name={"chevron-right"} size={12} />
