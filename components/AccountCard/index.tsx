ccba2502f19a090f0a88f1ef453d5337484b738a
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2021-11-17 15:14:02 +0100

    fix account card to match new card structure

--- components/AccountCard/index.tsx
+++ components/AccountCard/index.tsx
@@ -1,12 +1,14 @@
 import Button from "components/Button";
 import Card from "components/Card";
 import CardList from "components/CardList";
+import Text from "components/Text";
 import {
   accountCardHeadingText,
   emailText,
   nameText,
   signOutText,
 } from "constants/strings";
+import { tailwind } from "lib/tailwind";
 import React, { useEffect, useState } from "react";
 import useStore from "store";
 
@@ -36,8 +38,12 @@ const AccountCard = () => {
         actionText="Bearbeiten"
         onPressAction={() => {}}
       >
-        <Card title={nameText} value={firstName} />
-        <Card title={emailText} value={email} />
+        <Card title={nameText} value={firstName}>
+          <Text style={tailwind("text-xl")}>{firstName}</Text>
+        </Card>
+        <Card title={emailText} value={email}>
+          <Text style={tailwind("text-xl")}>{email}</Text>
+        </Card>
       </CardList>
       <Button text title="Abmelden" onPress={handleSignOut}>
         {signOutText}
