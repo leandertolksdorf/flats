f214fe90c4a8c07ce6f3fc452eb899e89136a8c3
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2021-11-17 14:08:00 +0100

    add type annotations to main tab navigator (#16)

--- navigation/Main/index.tsx
+++ navigation/Main/index.tsx
@@ -1,35 +1,31 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomTabScreenProps,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
 import { homeTabLabelText } from "constants/strings";
 import { BottomTabNavigatorScreenOptions } from "lib/themes";
 import HomeStack from "navigation/Home";
 import React from "react";
 
-const MainTab = createBottomTabNavigator();
+type MainTabParamList = {
+  Home: undefined;
+};
+
+export type MainTabHomeProps = BottomTabScreenProps<MainTabParamList, "Home">;
+
+const MainTab = createBottomTabNavigator<MainTabParamList>();
 
 const MainNavigator = () => {
   return (
     <MainTab.Navigator screenOptions={BottomTabNavigatorScreenOptions}>
       <MainTab.Screen
-        name="homeTab"
+        name="Home"
         component={HomeStack}
         options={{
           tabBarLabel: homeTabLabelText,
           headerTitle: homeTabLabelText,
         }}
       />
-      {/* <MainTab.Screen
-        name="tasksTab"
-        component={Tasks}
-        options={{
-          tabBarLabel: tasksTabLabelText,
-          headerTitle: tasksTabLabelText,
-        }}
-      /> */}
-      {/* <MainTab.Screen
-        name="finance"
-        component={HomeStack}
-        options={{ tabBarLabel: financeTabLabelText }}
-      /> */}
     </MainTab.Navigator>
   );
 };
